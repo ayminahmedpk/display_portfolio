e8c873e6ffe0f4ea07af0293093db186f4edc11b
ayminahmedpk <ayminahmedpk@gmail.com>
2022-02-19 12:45:25 +0500

    dev 0.0.2 - 'email done'

--- src/Pages/ContactPage/ContactPage.js
+++ src/Pages/ContactPage/ContactPage.js
@@ -1,14 +1,49 @@
 
 
-import React from 'react';
+import React, {useRef} from 'react';
+
+import emailjs from '@emailjs/browser';
+
+
+
+
+import './ContactPage.scss';
 
 
 const ContactPage = () => {
+
+    const emailjs_api_key = process.env.REACT_APP_EMAILJS_API_KEY;
+
+    const form = useRef();
+    
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_t0ast16', 'template_0oors83', form.current, emailjs_api_key)
+            .then((result) => { console.log(result.text); e.target.reset() },
+            (error)        => { console.log(error.text);  }
+        );
+
+    };
+
+
     return (
         <div className="content">
             <h2>
-                Contact
+                Contact Form
             </h2>
+
+            <form ref={form} onSubmit={sendEmail} className="mail-form">
+
+                <input type="text"  className="input" placeholder="Name"    name="name"   />
+                <input type="email" className="input" placeholder="Email"   name="email"  />
+                <input type="text"  className="input" placeholder="Subject" name="subject"/>
+
+                <textarea placeholder="Your message" name="message"/>
+
+                <input type="submit" value="SEND"/>
+
+            </form>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom';
 import App from './App/App.js';
 
 
+
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
